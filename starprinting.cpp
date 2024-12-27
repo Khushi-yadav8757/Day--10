2c05168e6361e9f4afebebec7aeb148b5ced452c
Khushi Yadav <kky957242@gmail.com>
2024-12-27 10:59:50 +0530

    Create starprinting.cpp

---
+++ starprinting.cpp
@@ -0,0 +1,20 @@
+*****
+*****
+*****
+*****
+pattern printing--
+  #include<iostrem>
+using namespace std;
+int main(){
+int row,col;
+for(row=1;row<=4;row++)
+{
+  for(col=1;col<=4;col++)
+{
+  cout<<"*"<<" ";
+  }
+}
+
+
+
+
