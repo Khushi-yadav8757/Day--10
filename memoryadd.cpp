1ae3de9bde2306dcd47c142e013e269eb5886cbe
Khushi Yadav <kky957242@gmail.com>
2024-12-27 09:44:34 +0530

    Create memoryadd.cpp

---
+++ memoryadd.cpp
@@ -0,0 +1,17 @@
+Memory Address
+In the example from the previous page, the & operator was used to create a reference variable. But it can also be used to get the memory address of a variable; which is the location of where the variable is stored on the computer.
+
+When a variable is created in C++, a memory address is assigned to the variable. And when we assign a value to the variable, it is stored in this memory address.
+
+To access it, use the & operator, and the result will represent where the variable is stored
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string food = "Pizza";
+
+  cout << &food;
+  return 0;
+}
