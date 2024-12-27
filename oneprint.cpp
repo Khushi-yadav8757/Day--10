de43904ad2c430cdc55a7baf4b854e1f52151eeb
Khushi Yadav <kky957242@gmail.com>
2024-12-27 11:13:08 +0530

    Create oneprint.cpp

---
+++ oneprint.cpp
@@ -0,0 +1,19 @@
+11111
+11111
+11111
+11111
+pattern printing----
+
+#include<iostream>
+using namespace std;
+int main(){
+int row,col;
+for(row=1;row<=5;row++)
+{
+   for(col=1;col<=5;col++)
+{ 
+  cout<<" 1 "<<" ";
+}
+   cout<<endl;
+}
+}
