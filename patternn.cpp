748b798e8a0c6a931cb0ff571242d6b82f7eff22
Khushi Yadav <kky957242@gmail.com>
2024-12-27 11:31:46 +0530

    Create patternn.cpp

---
+++ patternn.cpp
@@ -0,0 +1,18 @@
+54321
+54321
+54321
+54321
+pattern printing--
+#include<iostream>
+using namespace std;
+int main (){
+int row, col ;
+for (row=1;row<=5;row++)
+{ 
+  for(col=1;col>=1;col++)
+{   
+   cout<<col<<" ";
+}
+  count<<endl;
+}
+}
